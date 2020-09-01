aa50ebfc49ec73818c26ca535b53d7f4c0323c5a
Chase Redford <chaseredford@Chases-MacBook-Pro.local>
2020-08-31 23:32:40 -0600

    th

--- users/users-model.js
+++ users/users-model.js
@@ -1,41 +1,37 @@
 const db = require("../DBconfig");
 
 module.exports = {
-    add,
-    find,
-    findBy,
-    findById,
-    remove,
+  add,
+  find,
+  findBy,
+  findById,
+  remove,
 };
 
 function add(user) {
-
-    return db('users')
-
-        .insert(user)
+  return db("users").insert(user);
 }
 
 function find() {
-    return db('users').select('id', 'username')
-};
-
+  return db("users").select("id", "username");
+}
 
 function findBy(filter) {
-    return db("users").where(filter);
+  return db("users").where(filter);
 }
 
 function findById(id) {
-    return db("users")
-        .where({
-            id
-        })
-        .first();
+  return db("users")
+    .where({
+      id,
+    })
+    .first();
 }
 
 function remove(id) {
-    return db("users")
-        .where({
-            id
-        })
-        .del();
+  return db("users")
+    .where({
+      id,
+    })
+    .del();
 }
