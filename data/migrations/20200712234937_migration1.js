9c02207c3fc1d06d2f6baa46f3f2ce79176d6c9f
Chase Redford <chaseredford@Chases-MacBook-Pro.local>
2020-09-09 21:40:23 -0600

    a

--- data/migrations/20200712234937_migration1.js
+++ data/migrations/20200712234937_migration1.js
@@ -15,7 +15,7 @@ exports.up = function (knex) {
         .inTable("users")
         .onDelete("CASCADE");
       tbl.float("total_for_day");
-      tbl.timestamp(true, true);
+      tbl.timestamps(true, true);
     })
     .createTable("monthly_totals", (tbl) => {
       tbl.increments();
@@ -27,7 +27,7 @@ exports.up = function (knex) {
         .inTable("users")
         .onDelete("CASCADE");
       tbl.string("total_for_month");
-      tbl.timestamp(true, true);
+      tbl.timestamps(true, true);
     });
 };
 
